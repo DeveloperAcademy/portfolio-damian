0765e4231a31965472c7a93a653ccffd8dc09df1
Adrian Bury <kubaadrian2@gmail.com>
2016-11-05 14:34:24 +0100

    Initial framework page

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="pl">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Damian Terebun | Portfolio</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href=""/>
+
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!--screen CSS-->
+
+</head>
+<body>
+<!--content-->
+
+
+<!--jQuery -->
+<script src="bower_components/jquery/dist/jquery.min.js?ver=3.1.1"></script>
+<!--Bootstrap JS-->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js?ver=3.3.7"></script>
+<script src="javascript/app.js?ver=1.0.0"></script>
+</body>
+</html>
