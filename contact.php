6497c04fbe33d0adebc19ba24f054b4a7f372a71
chalvagit <clv.chalva@gmail.com>
2016-11-05 18:03:59 +0100

    added form

---
+++ contact.php
@@ -0,0 +1,7 @@
+<form action="mailto:damian.terebun@gmail.com" method="post">
+<label for="email">Email:</label><input type="email" name="email">
+<label for="name">Name:</label><input type="text" name="name">
+<label for="topic">Topic:</label><input type="text" name="topic">
+<label for="message">Message:</label><textarea name="message" rows="5" wrap="virtual"></textarea>
+<button type="submit">Send</button>
+</form>
