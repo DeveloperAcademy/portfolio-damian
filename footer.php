7f9b793cf68cecc5ffd12cadba4ebc61574d34a0
chalvagit <clv.chalva@gmail.com>
2016-11-07 00:40:54 +0100

    Added css for footer and contact form

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-<i class="icon-facebook-rect"></i>
+<footer class="footer-top"><i class="icon-facebook-rect"></i>
 <i class="icon-github"></i>
 <i class="icon-linkedin-rect"></i>
 <a href="#body" id="go-top"><span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span></a>
-<span>@DeveloperAcademy 2016 | All rights reserved</span>
-<!--jQuery -->
+<p><span>@DeveloperAcademy 2016 | All rights reserved</span></p>
+</footer><!--jQuery -->
 <script src="bower_components/jquery/dist/jquery.min.js?ver=3.1.1"></script>
 <!--Bootstrap JS-->
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js?ver=3.3.7"></script>
