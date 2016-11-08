5e4d866fd5014e3e82cb162514f8c762323f773d
chalvagit <clv.chalva@gmail.com>
2016-11-09 00:01:15 +0100

    added dummy img and empty modal

--- projects.php
+++ projects.php
@@ -1,1 +1,24 @@
 <h1>PROJECTS</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
+        <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg">
+        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
+        <img src="img/dummy_project_img.jpg">
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
+        <img src="img/dummy_project_img.jpg">
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
+        <img src="img/dummy_project_img.jpg">
+        </div>
+    </div>
+</div>
