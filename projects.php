0734798dc710a7f784652e46b805731eb5bf7e07
chalvagit <clv.chalva@gmail.com>
2016-11-09 23:55:22 +0100

    fixes in projects.php and css

--- projects.php
+++ projects.php
@@ -2,39 +2,42 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" />
-            <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header modal-top">
-                            <h4 class="modal-title">Project title</h4>
-                        </div>
-                        <div class="modal-body modal-inside">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2">
-                        <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="project-img-modal" />
-                                    </div>
-                                    <div class="col-xs-8 col-sm-6 col-md-9 col-lg-7">
+            <div class="project-title">Very very long Project title</div>
+            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" alt="project" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="project-title">Very very long Project title</div>
+            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" alt="project" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="project-title">Very very long Project title</div>
+            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" alt="project" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="project-title">Very very long Project title</div>
+            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" alt="project" />
+        </div>
+    </div>
+</div>
+
+
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header modal-top">
+                <h4 class="modal-title">Project title</h4>
+            </div>
+            <div class="modal-body modal-inside">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                        <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="project-img-modal center-block" />
+                    </div>
+                    <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
                         <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida vehicula sollicitudin. Vivamus viverra eu tellus eget rhoncus. Nullam lorem velit, sagittis nec eros ac, finibus interdum ante. Proin in pulvinar mauris, pellentesque blandit tellus. Proin lacinia massa augue, at posuere risus suscipit nec. Integer tincidunt odio sit amet convallis hendrerit. Donec ut eleifend est. Quisque vitae lacinia lectus, vel dictum nibh. Etiam diam nunc, venenatis vitae arcu ac, aliquet feugiat arcu.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer modal-bottom">
-                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
         </div>
     </div>
 </div>
