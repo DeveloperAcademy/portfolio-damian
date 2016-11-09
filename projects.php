e6406bdfe17fc76b6c10b04d4566fe6dbd462559
chalvagit <clv.chalva@gmail.com>
2016-11-09 22:02:38 +0100

    projects.php filled with temp content + css

--- projects.php
+++ projects.php
@@ -1,24 +1,40 @@
 <h1>PROJECTS</h1>
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
-        <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg">
-        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      ...
-    </div>
-  </div>
-</div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="center-block project-img" />
+            <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header modal-top">
+                            <h4 class="modal-title">Project title</h4>
+                        </div>
+                        <div class="modal-body modal-inside">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2">
+                        <img src="img/dummy_project_img.jpg" data-toggle="modal" data-target=".bd-example-modal-lg" class="project-img-modal" />
+                                    </div>
+                                    <div class="col-xs-8 col-sm-6 col-md-9 col-lg-7">
+                        <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida vehicula sollicitudin. Vivamus viverra eu tellus eget rhoncus. Nullam lorem velit, sagittis nec eros ac, finibus interdum ante. Proin in pulvinar mauris, pellentesque blandit tellus. Proin lacinia massa augue, at posuere risus suscipit nec. Integer tincidunt odio sit amet convallis hendrerit. Donec ut eleifend est. Quisque vitae lacinia lectus, vel dictum nibh. Etiam diam nunc, venenatis vitae arcu ac, aliquet feugiat arcu.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer modal-bottom">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
-        <img src="img/dummy_project_img.jpg">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
-        <img src="img/dummy_project_img.jpg">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-4 col-xl-3">
-        <img src="img/dummy_project_img.jpg">
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <img src="img/dummy_project_img.jpg" class="center-block project-img" />
         </div>
     </div>
 </div>
