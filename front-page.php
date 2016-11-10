069846b41bf7c5671dabb895c2f45c0275457063
Maadar <marcin171194@wp.pl>
2016-11-10 15:27:48 +0100

    add front-page.php

--- front-page.php
+++ front-page.php
@@ -1,1 +1,81 @@
-<h1>Front page</h1>
+<div class="container front-page_wallpaper">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="text-center">
+				<h1 class="front-page_logoText">Damian Terebun - programista Java</h1>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="container">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+					<li data-target="#myCarousel" data-slide-to="3"></li>
+				</ol>
+
+				<!-- Wrapper for slides -->
+				<div class="carousel-inner" role="listbox">
+					<div class="item active">
+						<img class="front-page_sliderStyles" src="img/doPortfolio1.png" alt="pictureSlider1">
+						
+						<div class="absolute-div">
+                        	<div class="carousel-caption">
+                        	    <h3 class="front-page_textWithoutBlur">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis doloribus autem eaque dolorum</h3>
+                        	</div>
+                    	</div>
+
+					</div>
+
+					<div class="item">
+						<img class="front-page_sliderStyles" src="img/doPortfolio2.jpg" alt="pictureSlider2">
+					
+						<div class="absolute-div">
+                        	<div class="carousel-caption">
+                        	    <h3 class="front-page_textWithoutBlur">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis doloribus autem eaque dolorum</h3>
+                        	</div>
+                    	</div>
+
+					</div>
+
+					<div class="item">
+						<img class="front-page_sliderStyles" src="img/doPortfolio3.jpg" alt="pictureSlider3">
+
+						<div class="absolute-div">
+                        	<div class="carousel-caption">
+                        	    <h3 class="front-page_textWithoutBlur">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis doloribus autem eaque dolorum</h3>
+                        	</div>
+                    	</div>
+
+					</div>
+
+					<div class="item">
+						<img class="front-page_sliderStyles" src="img/doPortfolio4.jpg" alt="pictureSlider4">
+					
+						<div class="absolute-div">
+                        	<div class="carousel-caption">
+                        	    <h3 class="front-page_textWithoutBlur">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis doloribus autem eaque dolorum</h3>
+                        	</div>
+                    	</div>
+
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 front-page-leftText">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque obcaecati perspiciatis repellat ea minima ipsa, modi aspernatur, sint minus ratione et assumenda, tempore inventore suscipit praesentium cupiditate rem laudantium a.
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<img class="img-circle front-page_damianPhoto" src="img/damian.jpg" alt="Damian photo">
+		</div>
+	</div>
+</div>
